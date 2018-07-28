07d10ee806ec7e2bc0ffbcd972540319f5d79640
izqalan <izqalan@gmail.com>
2018-07-29 01:45:56 +0800

    added program execution checking

--- datastructs/Header.h
+++ datastructs/Header.h
@@ -4,10 +4,17 @@
 #include <time.h>
 #include <sstream>
 #include <string>
+#define max 10
 
 using namespace std;
 
-#define max 10
+/**
+ * TODO
+ * Search function
+ * Update / edit record fucntion
+ */
+
+
 string filename;
 
 struct car{
@@ -26,6 +33,9 @@ typedef struct queue{
 }qq;
 
 car get(struct car *c){
+
+	// TODO year can only take numbers
+
 	cout << "Manufacture: ";
 	cin >> c->brand;
 	//cin->ignore();
@@ -42,7 +52,8 @@ car get(struct car *c){
 }
 
 string mkfile(){
-	// current time filename
+
+	// filename based on current time
 	char filename[50];
 	time_t getTime;
 	struct tm now;
@@ -77,22 +88,39 @@ void sav(struct queue *q){
 	//ss << "notepad.exe " << filename;
 	//char notepad[50];
 	//string s = ss.str();
-	//strcpy_s(notepad,filename.c_str());/
+	//strcpy_s(notepad,filename.c_str());
 
 	cout << "file saved: " <<filename.c_str() << endl;
 
 }
 
 void popup(){
-// apperently 'notepad.exe' is not required in system param
-	system(filename.c_str());
+	// will popup .txt file
+	// apperently 'notepad.exe' is not required in system param
+	
+	int result = system(filename.c_str());
+
+	// just incase some system require 'notepad.exe' in the system param function
+	if (result != 0){
+		
+		stringstream ss;
+		char notepad[50];
+		ss << "notepad.exe " << notepad;
+		strcat_s(notepad, filename.c_str());
+		int result = system(notepad);
+
+		if (result != 0){
+			cout << "save filed ... please contact the coders" << endl;
+		}
+	}
 }
 
 
 void display(struct queue *q){
 
+	// TODO need null cheking
+
 	for (int i = q->front; i <= q->rear; i++){
-		// need null checking
 		cout << "ID: " << q->data[i].id;
 		cout << " Manufacture: " << q->data[i].brand;
 		cout << " Model: " << q->data[i].model;
@@ -102,6 +130,9 @@ void display(struct queue *q){
 }
 
 void add(car *c, queue *q){
+
+	// TODO need to check queue memory availability
+
 	if (q->rear != -1){
 		c->nextId = q->rear + 1;
 	}
@@ -111,8 +142,10 @@ void add(car *c, queue *q){
 
 }
 
-// prob
 void del(int id, car *c, queue *q, queue *temp){
+
+	// After delete a record it will save the record.
+
 	cout << "deleting :" << id << endl;
 	c->id = id;
 	for (int i = 0; i <= q->rear; i++){
@@ -129,10 +162,4 @@ void del(int id, car *c, queue *q, queue *temp){
 	*q = *temp;
 	sav(q);
 }
-
-
-
-
-
-
 
